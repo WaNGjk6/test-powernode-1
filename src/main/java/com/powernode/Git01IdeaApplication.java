7c890482155233809bf372a940d61c67e341c854
changming <changming@qq.com>
2024-10-28 14:26:48 +0800

    初始化项目

---
+++ src/main/java/com/powernode/Git01IdeaApplication.java
@@ -0,0 +1,13 @@
+package com.powernode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git01IdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git01IdeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/powernode/Git01IdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powernode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git01IdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
